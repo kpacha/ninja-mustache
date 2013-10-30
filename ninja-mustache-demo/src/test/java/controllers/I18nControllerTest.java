419f2dfa958d23b5eb151024f90db4aacf4db480
Kpacha <kpacha666@gmail.com>
2013-10-30 22:35:42 +0100

    fixing the i18n tests

--- ninja-mustache-demo/src/test/java/controllers/I18nControllerTest.java
+++ ninja-mustache-demo/src/test/java/controllers/I18nControllerTest.java
@@ -30,8 +30,8 @@ import com.google.common.collect.Maps;
 
 public class I18nControllerTest extends NinjaTest {
     
-    String TEXT_EN = "Hello - this is an i18n message in the templating engine with placeholder: Yea!";
-    String TEXT_DE = "Hallo - das ist eine internationalisierte Nachricht in der Templating Engine mit placeholder: Yea!";
+    String TEXT_EN = "Hello - this is an i18n message in the templating engine : Yea!";
+    String TEXT_DE = "Hallo - das ist eine internationalisierte Nachricht in der Templating Engine: Yea!";
 
     @Test
     public void testThatI18nWorksEn() {
