305722480d69515d746cae61798667d7dddda270
Kpacha <kpacha666@gmail.com>
2013-11-01 20:20:44 +0100

    moving error templates to views/system

--- ninja-mustache-module/src/main/java/ninja/mustache/utils/MustacheConstant.java
+++ ninja-mustache-module/src/main/java/ninja/mustache/utils/MustacheConstant.java
@@ -24,6 +24,6 @@ package ninja.mustache.utils;
 public class MustacheConstant {
 
     public static final String DEFAULT_EXTENSION = ".mustache";
-    public static final String LOCATION_VIEW_HTML_INTERNAL_SERVER_ERROR = "views/error/500internalServerError"
+    public static final String LOCATION_VIEW_HTML_INTERNAL_SERVER_ERROR = "views/system/500internalServerError"
 	    + DEFAULT_EXTENSION;
 }
