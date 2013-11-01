0bed7fc75ececde36604ec619cdc055bfb55372f
Kpacha <kpacha666@gmail.com>
2013-11-01 23:43:47 +0100

    factory provider refactored

--- ninja-mustache-module/src/main/java/ninja/mustache/MustacheFactoryProvider.java
+++ ninja-mustache-module/src/main/java/ninja/mustache/MustacheFactoryProvider.java
@@ -1,6 +1,8 @@
 package ninja.mustache;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import ninja.utils.NinjaProperties;
 
@@ -23,20 +25,38 @@ public class MustacheFactoryProvider implements Provider<MustacheFactory> {
 
     private final Logger logger;
     private final NinjaProperties ninjaProperties;
-    private MustacheFactory cacheEnabledFactory = null;
+    private MustacheFactory cachedFactory = null;
+    private List<Object> resourceRoots = new ArrayList<Object>();
 
     @Inject
     public MustacheFactoryProvider(Logger logger,
 	    NinjaProperties ninjaProperties) {
 	this.logger = logger;
 	this.ninjaProperties = ninjaProperties;
+	initResourceRoots();
+    }
+
+    /**
+     * resgister the roots to be passed to the fallback factory
+     */
+    private void initResourceRoots() {
+	if (!ninjaProperties.isProd()) {
+	    File srcRoot = getSrcViewsRootFile();
+	    if (srcRoot.exists()) {
+		resourceRoots.add(srcRoot);
+	    }
+	}
+	resourceRoots.add("");
+	resourceRoots.add("../../");
+	logger.debug(resourceRoots.size()
+		+ " resource roots have been registered!");
     }
 
     @Override
     public MustacheFactory get() {
 	MustacheFactory factory = null;
 	if (!ninjaProperties.isDev()) {
-	    factory = getcacheEnabledFactory();
+	    factory = getCachedFactory();
 	} else {
 	    factory = getNewFallbackFactory();
 	}
@@ -49,11 +69,11 @@ public class MustacheFactoryProvider implements Provider<MustacheFactory> {
      * 
      * @return
      */
-    private MustacheFactory getcacheEnabledFactory() {
-	if (cacheEnabledFactory == null) {
-	    cacheEnabledFactory = getNewFallbackFactory();
+    private MustacheFactory getCachedFactory() {
+	if (cachedFactory == null) {
+	    cachedFactory = getNewFallbackFactory();
 	}
-	return cacheEnabledFactory;
+	return cachedFactory;
     }
 
     /**
@@ -63,18 +83,13 @@ public class MustacheFactoryProvider implements Provider<MustacheFactory> {
      * @return
      */
     private MustacheFactory getNewFallbackFactory() {
-	File srcRoot = getSrcViewsRootFile();
-	Object[] roots = null;
-	if (srcRoot.exists()) {
-	    roots = new Object[] { srcRoot, "", "../../" };
-	} else {
-	    roots = new Object[] { "", "../../" };
-	}
-	return new FallbackMustacheFactory(roots);
+	logger.debug("Instantiating a new Mustache Factory with "
+		+ resourceRoots.size() + " resource roots");
+	return new FallbackMustacheFactory(resourceRoots.toArray());
     }
 
     /**
-     * Init src folder for testing
+     * Init src folder
      * 
      * @return
      */
